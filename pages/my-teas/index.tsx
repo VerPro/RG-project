eb83124c778dc2428581acb9c7d947bb706002ee
Veronika Prokůpková <prokupkova.verca@gmail.com>
2022-04-04 23:53:58 +0200

    Add logic of the display of the index page in my-teas

--- pages/my-teas/index.tsx
+++ pages/my-teas/index.tsx
@@ -1,25 +1,76 @@
+import { Stack } from '@mui/material';
 import Button from '@mui/material/Button';
 import Link from 'next/link';
+import { useState } from 'react';
+import { db } from '../db';
 
 export default function MyTeas() {
-  return (
-    <>
-      <h1>Moje čajová polička</h1>
+  console.log(`databáze: ${db}`);
 
-      <Link href="/home">
-        <Button variant="outlined">
-          <a> Domů</a>
-        </Button>
-      </Link>
+  const [numberOfTeas, setNumberOfTeas] = useState(0);
 
-      <p>Zatím zde nemáš přidaný žádný čaj :-(</p>
-      <p>Můžeš začít tím, že si přidáš nějaký čaj</p>
+  //setNumberOfTeas(db.lenght) - tak nějak
 
-      <Link href="/add-tea">
-        <Button variant="outlined">
-          <a>Přidat čaj</a>
-        </Button>
-      </Link>
-    </>
-  );
+  const NoTeasScreen = () => {
+    return (
+      <Stack spacing={2}>
+        <h1>Moje čajová polička</h1>
+
+        <Link href="/home">
+          <Button variant="outlined">
+            <a> Domů</a>
+          </Button>
+        </Link>
+
+        <p>Zatím zde nemáš přidaný žádný čaj :-(</p>
+        <p>Můžeš začít tím, že si přidáš nějaký čaj</p>
+
+        <Link href="/add-tea">
+          <Button variant="outlined">
+            <a>Přidat čaj</a>
+          </Button>
+        </Link>
+      </Stack>
+    );
+  };
+
+  const TeaShelf = () => {
+    return (
+      <Stack spacing={2}>
+        <h1>Moje čajová polička</h1>
+
+        <Link href="/home">
+          <Button variant="outlined">
+            <a> Domů</a>
+          </Button>
+        </Link>
+
+        <Stack className="teas">
+          <h2>Čaje</h2>
+
+          <Link href="/my-teas">
+            <Button variant="outlined">Tie Guan Yin</Button>
+          </Link>
+
+          <Link href="/my-teas">
+            <Button variant="outlined">Darjeeling</Button>
+          </Link>
+          <Link href="/my-teas">
+            <Button variant="outlined">Sencha</Button>
+          </Link>
+        </Stack>
+        <Link href="/add-tea">
+          <Button variant="outlined">
+            <a>Přidat čaj</a>
+          </Button>
+        </Link>
+      </Stack>
+    );
+  };
+
+  if (numberOfTeas === 0) {
+    return NoTeasScreen();
+  } else {
+    return TeaShelf();
+  }
 }
