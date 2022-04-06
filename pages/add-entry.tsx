8a56553e8df10748517b27598d979b038e68f62d
Veronika Prokůpková <prokupkova.verca@gmail.com>
2022-04-07 00:08:39 +0200

    Add Date Picker, Stack and TextField

--- pages/add-entry.tsx
+++ pages/add-entry.tsx
@@ -1,9 +1,17 @@
+import { LocalizationProvider, DatePicker } from '@mui/lab';
+import DateAdapter from '@mui/lab/AdapterDayjs';
+import { Stack, TextField } from '@mui/material';
 import Button from '@mui/material/Button';
 import Link from 'next/link';
+import { useState } from 'react';
 
 export default function AddEntry() {
+  //DatePicker management - potřeba upravi, opakuje se
+  const [date, setDate] = useState<Date | null>(null);
+  //
+
   return (
-    <>
+    <Stack spacing={2}>
       <h1>Přidat zápis</h1>
 
       <Link href="/diary">
@@ -11,6 +19,18 @@ export default function AddEntry() {
           <a>Zpět</a>
         </Button>
       </Link>
-    </>
+
+      <TextField required id="outlined" label="Název záznamu"></TextField>
+      <LocalizationProvider dateAdapter={DateAdapter}>
+        <DatePicker
+          label="Datum zápisu"
+          value={date}
+          onChange={(newDate) => {
+            setDate(newDate);
+          }}
+          renderInput={(params) => <TextField {...params} />}
+        ></DatePicker>
+      </LocalizationProvider>
+    </Stack>
   );
 }
