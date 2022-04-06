3f2e04579645de65406a0620b2f36867a584e0e0
Veronika Prokůpková <prokupkova.verca@gmail.com>
2022-04-07 00:10:21 +0200

    Add TeaShelf component

---
+++ components/TeaShelf.tsx
@@ -0,0 +1,36 @@
+import { Stack, Button } from '@mui/material';
+import Link from 'next/link';
+
+export const TeaShelf = () => {
+  return (
+    <Stack spacing={2}>
+      <h1>Moje čajová polička</h1>
+
+      <Link href="/home">
+        <Button variant="outlined">
+          <a> Domů</a>
+        </Button>
+      </Link>
+
+      <Stack className="teas">
+        <h2>Čaje</h2>
+
+        <Link href="/my-teas">
+          <Button variant="outlined">Tie Guan Yin</Button>
+        </Link>
+
+        <Link href="/my-teas">
+          <Button variant="outlined">Darjeeling</Button>
+        </Link>
+        <Link href="/my-teas">
+          <Button variant="outlined">Sencha</Button>
+        </Link>
+      </Stack>
+      <Link href="/add-tea">
+        <Button variant="outlined">
+          <a>Přidat čaj</a>
+        </Button>
+      </Link>
+    </Stack>
+  );
+};
