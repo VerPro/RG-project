b80d6747e37d2cd75f47cce3b2e5243c57b4e09e
Veronika Prokůpková <prokupkova.verca@gmail.com>
2022-04-07 00:06:27 +0200

    Add Stack component

--- pages/diary.tsx
+++ pages/diary.tsx
@@ -1,25 +1,28 @@
 import Link from 'next/link';
 import Button from '@mui/material/Button';
+import { Stack } from '@mui/material';
 
 export default function Diary() {
   return (
     <>
-      <h1>Můj deník</h1>
+      <Stack>
+        <h1>Můj deník</h1>
 
-      <Link href="/home">
-        <Button variant="outlined">
-          <a> Domů</a>
-        </Button>
-      </Link>
+        <Link href="/home">
+          <Button variant="outlined">
+            <a> Domů</a>
+          </Button>
+        </Link>
 
-      <p>Zatím zde nemáš přidaný žádný deníkový zápis :-(</p>
-      <p>Můžeš začít tím, že si přidáš nějaký deníkový zápis</p>
+        <p>Zatím zde nemáš přidaný žádný deníkový zápis :-(</p>
+        <p>Můžeš začít tím, že si přidáš nějaký deníkový zápis</p>
 
-      <Link href="/add-entry">
-        <Button variant="outlined">
-          <a>Přidat zápis</a>
-        </Button>
-      </Link>
+        <Link href="/add-entry">
+          <Button variant="outlined">
+            <a>Přidat zápis</a>
+          </Button>
+        </Link>
+      </Stack>
     </>
   );
 }
