8b74ae04cb3b5350c39b5fa86ca51e1844947325
Veronika Prokůpková <prokupkova.verca@gmail.com>
2022-05-30 21:06:47 +0200

    Fix Navigation to the bottom of the page

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -6,6 +6,7 @@ import HomeIcon from '@mui/icons-material/Home';
 import PermIdentityIcon from '@mui/icons-material/PermIdentity';
 import QuestionMarkIcon from '@mui/icons-material/QuestionMark';
 import { useRouter } from 'next/router';
+import { Paper } from '@mui/material';
 
 export default function Navigation() {
   const router = useRouter();
@@ -16,18 +17,26 @@ export default function Navigation() {
 
   return (
     <Box sx={{ width: '1' }}>
-      <BottomNavigation showLabels>
-        <BottomNavigationAction
-          label="Domů"
-          icon={<HomeIcon />}
-          onClick={() => onLink('/home')}
-        />
-        <BottomNavigationAction
-          label="Profil"
-          icon={<PermIdentityIcon onClick={() => onLink('/profile')} />}
-        />
-        <BottomNavigationAction label="Nápověda" icon={<QuestionMarkIcon />} />
-      </BottomNavigation>
+      <Paper
+        sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }}
+        elevation={3}
+      >
+        <BottomNavigation showLabels>
+          <BottomNavigationAction
+            label="Domů"
+            icon={<HomeIcon />}
+            onClick={() => onLink('/home')}
+          />
+          <BottomNavigationAction
+            label="Profil"
+            icon={<PermIdentityIcon onClick={() => onLink('/profile')} />}
+          />
+          <BottomNavigationAction
+            label="Nápověda"
+            icon={<QuestionMarkIcon />}
+          />
+        </BottomNavigation>
+      </Paper>
     </Box>
   );
 }
